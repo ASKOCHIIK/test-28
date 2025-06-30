0ac1133e6625148c9450740c6094d19fd29b6a6e
Doolot2003 <doolotarinaliev@gmail.com>
2025-06-30 17:57:39 +0600

    Итог работы

--- test.py
+++ test.py
@@ -5,3 +5,19 @@ class Test:
 
     def info(self):
         return self.name, self.age
+
+
+class Person(Test):
+    def __init__(self):
+        super().__init__('Доолот', 22)
+
+
+class Pers(Test):
+    def __init__(self):
+        super().__init__('Asan', 23)
+
+
+p = Person()
+a = Pers()
+print(p.info())
+print(a.info())
